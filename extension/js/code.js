7378941f7c62956a514515cb03ff5fa9dbe764fa
AtomicInitial <143096875+AtomicInitial@users.noreply.github.com>
2023-09-16 15:55:21 +0000

    first online prototype
    Co-authored-by: notfenixio <NotFenixio@users.noreply.github.com>

--- extension/js/code.js
+++ extension/js/code.js
@@ -7,16 +7,23 @@ const socket = new WebSocket(
 
 console.log("Enabled TurboLive - JSON updater!");
 
+const emptyJSON = '{"targets":[{"isStage":true,"name":"Stage","variables":{"`jEk@4|i[#Fk?(8x)AV.-my variable":["my variable",0]},"lists":{},"broadcasts":{},"blocks":{},"comments":{},"currentCostume":0,"costumes":[{"name":"backdrop1","dataFormat":"svg","assetId":"cd21514d0531fdffb22204e0ec5ed84a","md5ext":"cd21514d0531fdffb22204e0ec5ed84a.svg","rotationCenterX":240,"rotationCenterY":180}],"sounds":[],"volume":100,"layerOrder":0,"tempo":60,"videoTransparency":50,"videoState":"on","textToSpeechLanguage":null},{"isStage":false,"name":"Sprite1","variables":{},"lists":{},"broadcasts":{},"blocks":{},"comments":{},"currentCostume":0,"costumes":[{"name":"costume1","bitmapResolution":1,"dataFormat":"svg","assetId":"592bae6f8bb9c8d88401b54ac431f7b6","md5ext":"592bae6f8bb9c8d88401b54ac431f7b6.svg","rotationCenterX":44,"rotationCenterY":44}],"sounds":[],"volume":100,"layerOrder":1,"visible":true,"x":0,"y":0,"size":100,"direction":90,"draggable":false,"rotationStyle":"all around"}],"monitors":[],"extensions":[],"meta":{"semver":"3.0.0","vm":"0.2.0","agent":""}}'
+let oldJSON = ''
+
 socket.onopen = () => {
   interval = setInterval(() => {
     let projectJSON = window.vm.toJSON();
-    socket.send(`{ name: "update", message: ${projectJSON} }`);
+    if (projectJSON != emptyJSON && projectJSON != oldJSON) {
+      socket.send(JSON.stringify({ name: "update", message: projectJSON }));
+    }
+    oldJSON = window.vm.toJSON();
   }, 500);
 
   socket.onmessage = (event) => {
-    if (event.data.name == "update") {
-      window.vm.loadProject(event.data.message);
-      console.log(event.data.message)
+    parsedJSON = JSON.parse(event.data);
+    if (parsedJSON.name == "update") {
+      window.vm.loadProject(parsedJSON.message);
+      console.log(parsedJSON.message);
     }
   };
 };
--- index.js
+++ index.js
@@ -20,19 +20,24 @@ const port = 3000;
 
 function emit(event, data, exclude) {
   ws.clients.forEach((e) => {
-    if (!exclude == e) {
-      e.send({ name: [event], message: data });
+    if (exclude != e) {
+      console.log({name: [event], message: data }.toString())
+      e.send(JSON.stringify({ name: [event], message: data }));
     }
   });
 }
 
+let newdata = '{"targets":[{"isStage":true,"name":"Stage","variables":{"`jEk@4|i[#Fk?(8x)AV.-my variable":["my variable",0]},"lists":{},"broadcasts":{},"blocks":{},"comments":{},"currentCostume":0,"costumes":[{"name":"backdrop1","dataFormat":"svg","assetId":"cd21514d0531fdffb22204e0ec5ed84a","md5ext":"cd21514d0531fdffb22204e0ec5ed84a.svg","rotationCenterX":240,"rotationCenterY":180}],"sounds":[],"volume":100,"layerOrder":0,"tempo":60,"videoTransparency":50,"videoState":"on","textToSpeechLanguage":null},{"isStage":false,"name":"Sprite1","variables":{},"lists":{},"broadcasts":{},"blocks":{},"comments":{},"currentCostume":0,"costumes":[{"name":"costume1","bitmapResolution":1,"dataFormat":"svg","assetId":"592bae6f8bb9c8d88401b54ac431f7b6","md5ext":"592bae6f8bb9c8d88401b54ac431f7b6.svg","rotationCenterX":44,"rotationCenterY":44}],"sounds":[],"volume":100,"layerOrder":1,"visible":true,"x":0,"y":0,"size":100,"direction":90,"draggable":false,"rotationStyle":"all around"}],"monitors":[],"extensions":[],"meta":{"semver":"3.0.0","vm":"0.2.0","agent":""}}'
+
 ws.on("connection", (socket) => {
   console.log("user connected");
+  socket.send(JSON.stringify({ name: "update", message: newdata.message }))
   socket.on("message", (data) => {
-    console.log("new data: " + data + "socket: " + socket);
-    let e = eval(data) 
-    if (e.name == "update") {
-      emit(e.name, e.message, socket);
+    newdata = JSON.parse(data);
+    console.log("new data: " + newdata);
+    if (newdata.name == "update") {
+      console.log("update");
+      emit(newdata.name, newdata.message, socket);
     }
   });
   socket.on("close", () => {
